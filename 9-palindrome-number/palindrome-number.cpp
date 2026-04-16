60d3e1637f3963f087d12d72bcc7e46c67c03b2e
dwarkesh-creator <65116950+dwarkesh-creator@users.noreply.github.com>
2026-04-16 06:13:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.26%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // LeetCode yahan bool maangta hai kyunki answer true/false mein dena hai
+    bool isPalindrome(int x) {
+        
+        
+        if (x < 0) {
+            return false;
+        }
+        
+        
+        int temp = x;
+        long long rev = 0;
+        
+        while(x != 0) {
+            int digit = x % 10;
+            rev = (rev * 10) + digit;
+            x = x / 10;
+        } 
+        
+       
+        if (temp == rev) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+};
